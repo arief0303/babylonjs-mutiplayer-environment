1e546fc8ba70a0788ea69054c1ef45e85dab4be1
Arief R <arief0303@gmail.com>
2023-05-26 14:06:03 +0700

    mobile joystick v1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,80 @@
-<template>
-  <div class="fullscreen-container">
-    <button class="fullscreen-button" @click="toggleFullscreen">
-      {{ isFullscreen ? 'Exit Fullscreen' : 'Fullscreen' }}
-    </button>
-    <BabylonScene />
-  </div>
-</template>
-
 <script>
 import { defineComponent } from 'vue';
 import BabylonScene from './components/BabylonScene.vue'
+import { BeakerIcon, Cog6ToothIcon } from '@heroicons/vue/24/solid';
 
 export default defineComponent({
   components: {
     BabylonScene,
+    BeakerIcon,
+    Cog6ToothIcon
   },
+  setup() {
+    /* isFullscreen: false,
+      toggleFullscreen() {
+      this.isFullscreen = !this.isFullscreen;
+      if (this.isFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    }, */
+    /* const toggleFullscreen = () => {
+      isFullscreen.value = !isFullscreen.value;
+
+      if (isFullscreen.value) {
+        // Enter fullscreen mode
+        if (document.documentElement.requestFullscreen) {
+          document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+          document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+          document.documentElement.msRequestFullscreen();
+        }
+      } else {
+        // Exit fullscreen mode
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      }
+    }; */
+  }
 });
 </script>
+
+<template>
+  <div class="fullscreen-container">
+    <!-- <button class="fullscreen-button" @click="toggleFullscreen">
+      {{ isFullscreen ? 'Exit Fullscreen' : 'Fullscreen' }}
+    </button> -->
+    <BabylonScene />
+  </div>
+  <!-- <BeakerIcon class="icon" /> -->
+  <!-- <button @click="toggleFullscreen" class="icon">
+    <Cog6ToothIcon class="icon" />
+  </button> -->
+  <button class="fullscreen-button" @click="toggleFullscreen">
+    <!-- {{ isFullscreen ? 'Exit Fullscreen' : 'Fullscreen' }} -->
+    <Cog6ToothIcon class="icon" />
+  </button>
+</template>
+
+<style>
+.icon {
+  width: 36px;
+  height: 36px;
+  color: #222;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  margin: 15px;
+}
+</style>
---
+++ src/components/Settings.vue
@@ -0,0 +1,25 @@
+<script>
+import { BeakerIcon, Cog6ToothIcon } from '@heroicons/vue/24/solid';
+export default {
+    components: { BeakerIcon, Cog6ToothIcon }
+}
+</script>
+
+<template>
+    <div>
+        <beaker-icon class="icon"/>
+        <cog-6-tooth-icon class="icon"/>
+    </div>
+</template>
+
+<style>
+.icon {
+    width: 36px;
+    height: 36px;
+    color: #222;
+    margin-left: 12px;
+    right: 0;
+    bottom: 0;
+    z-index: 100;
+}
+</style>
